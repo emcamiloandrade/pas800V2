e42eeaa22c41c06f3384a249b9d8c253bc6c3bd4
emcamiloandrade <camilo.andrade@energymaster.co>
2025-12-10 16:13:48 -0500

    cambio ruta guardado

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
- 
+import requests
 app = Flask(__name__)
 auth = HTTPBasicAuth()
  
@@ -37,7 +37,38 @@ def recibir_archivo():
  
     return jsonify({"mensaje": f"Archivo {archivo.filename} guardado en {carpeta_destino}"}), 200
  
- 
+
+@app.route('/telemetry_seriot', methods=['POST'])
+def recibir_peticion_seriot():
+    try:
+        datos = request.get_json()
+        payload = {
+            "device": datos['device'],
+            "date": datos['date'],
+            "Activa": datos['Activa'],
+            "Reactiva": datos['Reactiva'],
+        }
+        header = {
+            "X-Auth-Token": request.headers.get('X-Auth-Token')
+        }
+        print("Entre a enviar info a NOVA")
+        response = requests.post(
+            url="https://canovabackendprodenergym.happysmoke-127484da.eastus2.azurecontainerapps.io/telemetry/seriot/load",
+            headers=header,
+            data=payload,
+        )
+        print("Despues de enviar datos a NOVA")
+        print("Me respondio: ", response.status_code)
+        if response.status_code in [200,201,202]:
+            return jsonify({"mensaje": f"Dato almacenado correctamente, {response.content}"}), 200
+        else:
+            return jsonify({"mensaje": "Dato no almacenado"}), 400
+    except Exception as e:
+        print(str(e))
+        return jsonify({"mensaje": "Problemas al procesar datos"}), 500
+
+
 if __name__ == '__main__':
     app.run()
+  
  
